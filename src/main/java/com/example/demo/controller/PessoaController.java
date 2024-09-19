cda3bf129bd02669feaf8dc0880c098e8bd72241
Jonas Lima <105572182+Jonaslima07@users.noreply.github.com>
2024-09-19 14:50:24 -0300

    adicionado os metodos PUT e DELETE

--- src/main/java/com/example/demo/controller/PessoaController.java
+++ src/main/java/com/example/demo/controller/PessoaController.java
@@ -3,11 +3,15 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 // import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 // import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 // import org.springframework.web.server.ResponseStatusException;
@@ -29,7 +33,36 @@ public class PessoaController {
 
 
     @PostMapping("/pessoas")
-    public Pessoa adicionarPessoa(@RequestBody Pessoa pessoa) {
+    public Pessoa adicionarPessoa(@RequestBody Pessoa pessoa) { //envia dados para adicionar pessoas
         return repository.save(pessoa);
     }
+
+    @PutMapping("/pessoas") // atualiza os dados de pessoas
+    public Pessoa atualizaPessoa(@RequestBody Pessoa pessoa){
+        return repository.save(pessoa);
+    }
+
+    @DeleteMapping("/pessoa/{id}")
+public ResponseEntity<Void> deletarPessoa(@PathVariable String id) {
+    // Verifique se a pessoa existe antes de tentar deletar
+    if (!repository.existsById(id)) {
+        return ResponseEntity.notFound().build();
+    }
+    
+    repository.deleteById(id);
+    return ResponseEntity.noContent().build(); // Retorna 204 No Content
 }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
