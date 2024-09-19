83460641fbb00addef05aba8e99067de95cede3c
HenrIcosta16 <154642352+HenrIcosta16@users.noreply.github.com>
2024-09-19 20:20:40 -0300

    Atualizar o controller

---
+++ src/main/java/com/example/demo/controller/MedicoController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.model.Medico;
+import com.example.demo.repository.MedicoRepository;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
+
+    @GetMapping("/medicos")
+    public Iterable<Medico> listarMedicos() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/medicos")
+    public Medico adicionarMedico(@RequestBody Medico medico) {
+        return repository.save(medico);
+    }
+
+    @PutMapping("/medicos")
+    public Medico atualizarMedico(@RequestBody Medico medico) {
+        return repository.save(medico);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletarMedico(@PathVariable String id) {
+        if (!repository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        repository.deleteById(id);
+        return ResponseEntity.ok("Médico deletado com sucesso");
+    }
+}
